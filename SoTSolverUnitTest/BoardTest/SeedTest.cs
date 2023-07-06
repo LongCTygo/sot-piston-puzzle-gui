1f35f7624907bc88c37eff5696d9c1248f911730
LongCT_ <thanhlong2704031@gmail.com>
2023-07-06 15:40:14 +0700

    Included the database into the project

--- SoTSolverUnitTest/BoardTest/SeedTest.cs
+++ SoTSolverUnitTest/BoardTest/SeedTest.cs
@@ -10,10 +10,37 @@ namespace SoTSolverUnitTest.BoardTest
         }
 
         [Test]
-        public void Test1()
+        public void UnitTest_1()
         {
-            Board board = new Board(1234);
-            Assert.AreEqual(1234,board.GetSeed());
+            Assert.Throws<ArgumentException>((() => new Board(-10)));
+        }
+
+        [Test]
+        public void UnitTest_2()
+        {
+            Assert.Throws<ArgumentException>((() => new Board(-1)));
+        }
+
+        [Test]
+        public void UnitTest_3()
+        {
+            int seed = 0;
+            Board b  = new Board(seed);
+            Assert.AreEqual(seed,b.Seed);
+        }
+
+        [Test]
+        public void UnitTest_4()
+        {
+            int seed = 8388607;
+            Board b = new Board(seed);
+            Assert.AreEqual(seed, b.Seed);
+        }
+
+        [Test]
+        public void UnitTest_5()
+        {
+            Assert.Throws<ArgumentException>((() => new Board(8388608)));
         }
     }
 }
